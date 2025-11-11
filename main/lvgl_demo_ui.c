b89453dc888034362289d0b1c11c685f364ad021
Barryblueice <barryblueice@outlook.com>
2025-11-11 16:14:57 +0800

    lvgl测试

---
+++ main/lvgl_demo_ui.c
@@ -0,0 +1,11 @@
+#include "lvgl.h"
+
+void example_lvgl_demo_ui(lv_display_t *disp)
+{
+    lv_obj_t *scr = lv_display_get_screen_active(disp);
+    lv_obj_t *label = lv_label_create(scr);
+    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
+    lv_label_set_text(label, "Hello LVGL.");
+    lv_obj_set_width(label, lv_display_get_horizontal_resolution(disp));
+    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);
+}
